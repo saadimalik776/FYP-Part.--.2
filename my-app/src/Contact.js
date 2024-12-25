0f4c6f0e4764e13bb00f3a2c306da9a21ed8c21a
Saad-rizwan320 <37968@students.riphah.edu.pk>
2024-12-25 22:38:54 +0500

    Update Contact.js (Add fontsome)

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import './contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import Font Awesome component
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
 
 function Contact() {
   return (
     <div className="container">
       <h1>Contact Us</h1>
-      <p>Email: sm019213@gmail.com</p>
-      <p>Phone: 923312921133</p>
-      <p>Address: 123 Eye Scanner St., Vision City</p>
+      <p>
+        <FontAwesomeIcon icon={faEnvelope} /> Email: sm019213@gmail.com
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faPhone} /> Phone: +92 331 2921133
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faMapMarkerAlt} /> Address: 123 Eye Scanner St., Vision City
+      </p>
     </div>
   );
 }
